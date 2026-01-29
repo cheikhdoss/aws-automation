e208e16afbdc3e9d893792594a4aa62aac57fbc9
cheikhdoss <cmbodj@groupeisi.com>
2026-01-29 01:17:58 +0000

    Fix secret_access_key and update AMI IDs for us-west-1

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region            = var.region
   access_key        = var.access_key
-  secret_key = var.secret_access_key
+  secret_access_key = var.secret_access_key
 }
 
 resource "aws_instance" "ec2_demo" {
--- infra/variables.tf
+++ infra/variables.tf
@@ -14,7 +14,7 @@ variable "secret_access_key" {
 
 variable "ami" {
   type    = string
-  default = "ami-04f34746e5e1ec0fe"
+  default = "ami-038bba9a164eb3dc1"  # Ubuntu 22.04 LTS for us-west-1
 }
 
 variable "instance_type" {
